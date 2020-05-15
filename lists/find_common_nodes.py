1d6355a11f3a628e7c064d6718ab2c6da0f04781
Moritz <moritz.mistol@web.de>
2020-05-15 23:21:54 +0200

    add docstrings to find_common_nodes.py

--- lists/find_common_nodes.py
+++ lists/find_common_nodes.py
@@ -2,6 +2,14 @@ from lists.node import Node
 
 
 def has_common_node(n1: Node, n2: Node):
+    """
+    Checks if two lists have a common node.
+
+    Two lists have a common node if one node points to another node in the other list.
+    :param n1: the head of the first list
+    :param n2: the head of the second list
+    :return: true if they have a common node
+    """
     while n1 is not None or n2 is not None:
         if n1 is not None:
             n1 = n1.next
@@ -10,15 +18,26 @@ def has_common_node(n1: Node, n2: Node):
     return n1 == n2
 
 
-def get_size(node: Node):
+def get_size(head: Node):
+    """
+    Returns the size of a list
+    :param head: the head of the list
+    :return: the size of the list
+    """
     count = 0
-    while node is not None:
+    while head is not None:
         count += 1
-        node = node.next
+        head = head.next
     return count
 
 
 def get_nth_node(head: Node, n: int):
+    """
+    Returns the nth node of the list
+    :param head: the head of the list
+    :param n: the nth node
+    :return: the nth node
+    """
     current = 0
     while current < n:
         if head is None:
@@ -39,6 +58,12 @@ def find_common_helper(n1, n2):
 
 
 def find_first_common_node(n1, n2):
+    """
+    Returns the first common node of two lists.
+    :param n1: the head of the first list
+    :param n2: the head of the second list
+    :return: the first common node of two lists
+    """
     size1 = get_size(n1)
     size2 = get_size(n2)
     diff = abs(size1 - size2)
