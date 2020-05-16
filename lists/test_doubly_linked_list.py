f46d5fd630f69bc49cf25e549f4ba3ff0f14c98d
Moritz <moritz.mistol@web.de>
2020-05-16 19:01:21 +0200

    add test_doubly_linked_list.py dummy

---
+++ lists/test_doubly_linked_list.py
@@ -0,0 +1,22 @@
+from unittest import main, TestCase
+
+
+class DoublyLinkedListTestCase(TestCase):
+    def test_splice(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+    def test_remove(self):
+        pass
+
+    def test_concat(self):
+        pass
+
+    def test_clear_all(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
