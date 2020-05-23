835902f39a6e0aa4648591569d50c17e38277550
Moritz <moritz.mistol@web.de>
2020-05-23 19:56:53 +0200

    add another variant of count_screws.py with a dict

--- hashing/count_screws_with_hash_table.py
+++ hashing/count_screws_with_hash_table.py
@@ -1,4 +1,6 @@
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Dict
+from random import randint
+from pprint import pprint
 
 
 class Screw(NamedTuple):
@@ -10,5 +12,28 @@ class Screw(NamedTuple):
     coils: int
 
 
-def count_screws(screws: List[Screw]) -> List[List[int]]:
-    pass
+def count_screws(screws: List[Screw]) -> Dict[Screw, int]:
+    """
+    Counts the number of each screw type.
+
+    :param screws: a list of screws
+    :return: a dict containing the number of screws per type
+    """
+    counts = {}
+    for screw in screws:
+        if screw in counts:
+            counts[screw] += 1
+        else:
+            counts[screw] = 1
+    return counts
+
+
+def test():
+    screws = [Screw(i % 2, (i + 1) % 2, (i * 3) % 2) for i in range(30)]
+    screws.extend(
+        [Screw(randint(0, 3), randint(0, 3), randint(0, 3)) for _ in range(100)])
+    pprint(count_screws(screws))
+
+
+if __name__ == '__main__':
+    test()
